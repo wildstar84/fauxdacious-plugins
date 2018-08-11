3a8c099b010c79ebc006d5780751447d32fa4e1e
Jim Turner <turnerjw784@yahoo.com>
2018-08-10 20:30:29 -0500

    search-tool: Add option to scan for new files at startup. Addresses Audacious bug#796 (Audacious commit# 5e2f89d).

--- src/search-tool/search-tool.cc
+++ src/search-tool/search-tool.cc
@@ -65,6 +65,7 @@ static void trigger_search ();
 
 const char * const SearchTool::defaults[] = {
     "max_results", "20",
+    "rescan_on_startup", "FALSE",
     nullptr
 };
 
@@ -72,6 +73,8 @@ const PreferencesWidget SearchTool::widgets[] = {
     WidgetSpin (N_("Number of results to show:"),
         WidgetInt (CFG_ID, "max_results", trigger_search),
          {10, 10000, 10}),
+    WidgetCheck (N_("Rescan library at startup"),
+        WidgetBool (CFG_ID, "rescan_on_startup"))
 };
 
 const PluginPreferences SearchTool::prefs = {{widgets}};
@@ -529,6 +532,8 @@ static void search_init ()
 
     update_database ();
 
+    if (aud_get_bool (CFG_ID, "rescan_on_startup"))
+        begin_add (get_uri ());
     hook_associate ("playlist add complete", add_complete_cb, nullptr);
     hook_associate ("playlist scan complete", scan_complete_cb, nullptr);
     hook_associate ("playlist update", playlist_update_cb, nullptr);
