62433da085445c4dea24a1318b1502da75d588f5
Jim Turner <turnerjw784@yahoo.com>
2018-08-15 13:36:47 -0500

    playlist-qt: Add song-info popup to QT (already on GTK). (Audacious commit# 592e3d6)

--- src/qtui/playlist-qt.cc
+++ src/qtui/playlist-qt.cc
@@ -24,9 +24,8 @@
 
 #include <libfauxdcore/audstrings.h>
 #include <libfauxdcore/drct.h>
-#include <libfauxdcore/hook.h>
-#include <libfauxdcore/playlist.h>
 #include <libfauxdcore/runtime.h>
+#include <libfauxdqt/libfauxdqt.h>
 
 #include "playlist-qt.h"
 #include "playlist_header.h"
@@ -57,6 +56,7 @@ PlaylistWidget::PlaylistWidget (QWidget * parent, int playlist) :
     setFrameShape (QFrame::NoFrame);
     setSelectionMode (ExtendedSelection);
     setDragDropMode (DragDrop);
+    setMouseTracking (true);
 
     updateSettings ();
     header->updateColumns ();
@@ -149,6 +149,23 @@ void PlaylistWidget::mouseDoubleClickEvent (QMouseEvent * event)
         playCurrentIndex ();
 }
 
+void PlaylistWidget::mouseMoveEvent (QMouseEvent * event)
+{
+    int row = indexToRow (indexAt (event->pos ()));
+    if (row < 0)
+    {
+        hidePopup ();
+        return;
+    }
+    if (aud_get_bool (nullptr, "show_filepopup_for_tuple") && m_popup_pos != row)
+        triggerPopup (row);
+}
+
+void PlaylistWidget::leaveEvent (QEvent *)
+{
+    hidePopup ();
+}
+
 /* Since Qt doesn't support both DragDrop and InternalMove at once,
  * this hack is needed to set the drag icon to "move" for internal drags. */
 void PlaylistWidget::dragMoveEvent (QDragMoveEvent * event)
@@ -219,7 +236,7 @@ void PlaylistWidget::scrollToCurrent (bool force)
 {
     int entry = aud_playlist_get_position (m_playlist);
 
-    if (aud_get_bool ("qtui", "autoscroll") || force)
+    if (entry >= 0 && (aud_get_bool ("qtui", "autoscroll") || force))
     {
         aud_playlist_select_all (m_playlist, false);
         aud_playlist_entry_set_selected (m_playlist, entry, true);
@@ -397,6 +414,26 @@ void PlaylistWidget::moveFocus (int distance)
     int row = currentIndex ().row ();
     row = aud::clamp (row + distance, 0, visibleRows - 1);
     setCurrentIndex (proxyModel->index (row, 0));
+}
+
+void PlaylistWidget::showPopup ()
+{
+    audqt::infopopup_show (m_playlist, m_popup_pos);
+}
+
+void PlaylistWidget::triggerPopup (int pos)
+{
+    audqt::infopopup_hide ();
+    m_popup_pos = pos;
+    m_popup_timer.queue (aud_get_int (nullptr, "filepopup_delay") * 100,
+     aud::obj_member<PlaylistWidget, & PlaylistWidget::showPopup>, this);
+}
+
+void PlaylistWidget::hidePopup ()
+{
+    audqt::infopopup_hide ();
+    m_popup_pos = -1;
+    m_popup_timer.stop ();
 }
 
 void PlaylistWidget::updateSettings ()
