75b1df555c5f5dcf1e178abdbad3be59c20bb6d7
Jim Turner <turnerjw784@yahoo.com>
2019-03-06 23:05:30 -0600

    m3u: Add support for "Extended M3U" format

--- src/m3u/m3u.cc
+++ src/m3u/m3u.cc
@@ -18,19 +18,29 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#include <stdlib.h>
 #include <string.h>
 #include <glib.h>  /* for g_get_current_dir, g_path_is_absolute */
 
+#include <libfauxdcore/preferences.h>
 #include <libfauxdcore/audstrings.h>
 #include <libfauxdcore/i18n.h>
 #include <libfauxdcore/plugin.h>
+#include <libfauxdcore/runtime.h>
 
 static const char * const m3u_exts[] = {"m3u", "m3u8", "txt", "ls"};  // JWT:ACCEPT .txt & "ls * |"
 
 class M3ULoader : public PlaylistPlugin
 {
 public:
-    static constexpr PluginInfo info = {N_("M3U Playlists"), PACKAGE};
+    static const PreferencesWidget widgets[];
+    static const PluginPreferences prefs;
+    static constexpr PluginInfo info = {
+        N_("M3U Playlists"),
+        PACKAGE,
+        nullptr,
+        & prefs
+    };
 
     constexpr M3ULoader () : PlaylistPlugin (info, m3u_exts, true) {}
 
@@ -38,6 +48,10 @@ public:
      Index<PlaylistAddItem> & items);
     bool save (const char * filename, VFSFile & file, const char * title,
      const Index<PlaylistAddItem> & items);
+
+private:
+    bool Extended_m3u = false;
+    Tuple tuple;
 };
 
 EXPORT M3ULoader aud_plugin_instance;
@@ -59,6 +73,9 @@ static char * split_line (char * line)
 bool M3ULoader::load (const char * filename, VFSFile & file, String & title,
  Index<PlaylistAddItem> & items)
 {
+    Extended_m3u = false;
+    tuple = Tuple ();
+
     Index<char> text = file.read_all ();
     if (! text.len ())
         return false;
@@ -69,6 +86,7 @@ bool M3ULoader::load (const char * filename, VFSFile & file, String & title,
     if (! strncmp (parse, "\xef\xbb\xbf", 3)) /* byte order mark */
         parse += 3;
 
+    bool firstline = true;
     while (parse)
     {
         char * next = split_line (parse);
@@ -76,25 +94,80 @@ bool M3ULoader::load (const char * filename, VFSFile & file, String & title,
         while (* parse == ' ' || * parse == '\t')
             parse ++;
 
-        if (* parse && * parse != '#')
+        if (* parse)
         {
-            if (strncmp (filename, "stdin://", 8))
+            if (* parse != '#')
             {
-                StringBuf s = uri_construct (parse, filename);
-                if (s)
-                    items.append (String (s));
+                String s;
+                if (! strncmp (filename, "stdin://", 8))  // WE'RE PIPING IN FROM STDIN:
+                {
+                    char * cur = g_get_current_dir ();
+                    String cur_path = String (filename_to_uri (filename_build ({cur, filename+8})));
+                    s = String (uri_construct (parse, cur_path));
+                    g_free (cur);
+                }
+                else
+                    s = String (uri_construct (parse, filename));
+
+                if (s && s[0])
+                {
+                    if (! strncmp (s, "file://", 7) && strstr (parse, "cue?"))  // CUESHEET:
+                        s = String (str_decode_percent (s));  // UNESCAPE THE "?" ESCAPED BY uri_construct ().
+
+                    if (Extended_m3u)
+                    {
+                        tuple.set_filename (s);
+                        items.append (String (s), std::move (tuple));  // NOTE:NEVER SET TUPLE VALID (FORCE RESCAN)!
+                    }
+                    else
+                        items.append (String (s));
+                }
             }
-            else
+            else if (Extended_m3u && ! strncmp (parse, "#EXTINF", 7))  // WE'RE A TITLE LINE (EXTENDED M3U):
             {
-                char * cur = g_get_current_dir ();
-                String cur_path = String (filename_to_uri (filename_build ({cur, filename+8})));
-                StringBuf s = uri_construct (parse, cur_path);
-                if (s)
-                    items.append (String (s));
-                g_free (cur);
+                tuple = Tuple ();
+                parse += 7;
+                if (parse < next && * parse == ':')
+                {
+                    ++parse;
+                    while (parse < next && * parse == ' ')
+                        ++parse;
+
+                    if (* parse && parse < next)
+                    {
+                        Index<String> headerparts = str_list_to_index (parse, ",");
+                        if (headerparts.len () > 1)
+                        {
+                            int tlen = atoi (headerparts[0]) * 1000;
+                            if (tlen <= 0)
+                                tuple.unset (Tuple::Length);
+                            else
+                                tuple.set_int (Tuple::Length, tlen);
+
+                            // FIND THE TITLE AND MOVEPAST ANY LEADING SPACES IN IT:
+                            char * c = parse;
+                            while (c < next && * c != ',')
+                                ++c;
+                            if (c < next && * c)
+                                ++c;
+                            while (c < next && * c == ' ')
+                                ++c;
+                            if (* c)
+                                tuple.set_str (Tuple::Title, c);
+                        }
+                        else if (headerparts.len () > 0)
+                        {
+                            tuple.unset (Tuple::Length);
+                            tuple.set_str (Tuple::Title, headerparts[0]);
+                        }
+                    }
+                }
             }
+            else if (firstline && ! strncmp (parse, "#EXTM3U", 7))  // WE'RE AN EXTENDED M3U:
+                Extended_m3u = true;
         }
 
+        firstline = false;
         parse = next;
     }
 
@@ -104,9 +177,25 @@ bool M3ULoader::load (const char * filename, VFSFile & file, String & title,
 bool M3ULoader::save (const char * filename, VFSFile & file, const char * title,
  const Index<PlaylistAddItem> & items)
 {
+    Extended_m3u = aud_get_bool ("m3u", "saveas_extended_m3u");
+    if (Extended_m3u && file.fwrite (str_copy("#EXTM3U\n"), 1, 8) != 8)
+        return false;
+
     for (auto & item : items)
     {
         StringBuf path = uri_deconstruct (item.filename, filename);
+        if (Extended_m3u && item.tuple.state () == Tuple::Valid)
+        {
+            int tuplen = item.tuple.get_int (Tuple::Length);
+            if (tuplen >= 0)
+                tuplen /= 1000;
+            String tupstr = item.tuple.get_str (Tuple::Title);
+            if (!tupstr)
+                tupstr = String (filename_get_base (item.filename));
+            StringBuf line = str_printf ("#EXTINF:%d, %s\n", tuplen, (const char *) tupstr);
+            if (file.fwrite (line, 1, line.len ()) != line.len ())
+                return false;
+        }
         StringBuf line = str_concat ({path, "\n"});
         if (file.fwrite (line, 1, line.len ()) != line.len ())
             return false;
@@ -114,3 +203,10 @@ bool M3ULoader::save (const char * filename, VFSFile & file, const char * title,
 
     return true;
 }
+
+const PreferencesWidget M3ULoader::widgets[] = {
+    WidgetLabel(N_("<b>M3U Configuration</b>")),
+    WidgetCheck(N_("Save in Extended M3U format?"), WidgetBool("m3u", "saveas_extended_m3u")),
+};
+
+const PluginPreferences M3ULoader::prefs = {{widgets}};
