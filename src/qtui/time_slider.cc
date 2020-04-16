f3bfad4487a9164f876026479eb2471f78986d5d
Jim Turner <turnerjw784@yahoo.com>
2020-04-16 16:41:12 -0500

    Aud: qtui: Fix memory leak, timeslider issues. (Audacious commits# fda439a, 3ead981, and 2a749bb)

--- src/qtui/time_slider.cc
+++ src/qtui/time_slider.cc
@@ -28,10 +28,7 @@
 #include <QProxyStyle>
 #include <QStyle>
 
-TimeSliderLabel::TimeSliderLabel (QWidget * parent) : QLabel (parent)
-{
-    setStyleSheet ("font-weight: bold");
-}
+TimeSliderLabel::TimeSliderLabel (QWidget * parent) : QLabel (parent) {}
 TimeSliderLabel::~TimeSliderLabel () {}
 
 void TimeSliderLabel::mouseDoubleClickEvent (QMouseEvent * event)
@@ -62,12 +59,12 @@ public:
 };
 
 TimeSlider::TimeSlider (QWidget * parent) :
-    QSlider (Qt::Horizontal, parent),
-    m_label (new TimeSliderLabel (parent))
+    QSlider (Qt::Horizontal, parent), m_label (new TimeSliderLabel(parent)),
+            m_style (new TimeSliderStyle)
 {
     setFocusPolicy (Qt::NoFocus);
     setSizePolicy (QSizePolicy::Expanding, QSizePolicy::Fixed);
-    setStyle (new TimeSliderStyle ());
+    setStyle (m_style);
 
     m_label->setContentsMargins (audqt::sizes.FourPt, 0, 0, 0);
     m_label->setSizePolicy (QSizePolicy::Fixed, QSizePolicy::MinimumExpanding);
@@ -79,19 +76,35 @@ TimeSlider::TimeSlider (QWidget * parent) :
     start_stop ();
 }
 
-TimeSlider::~TimeSlider () {}
+TimeSlider::~TimeSlider () { delete m_style; }
 
 void TimeSlider::set_label (int time, int length)
 {
     QString text;
 
     if (length >= 0)
+    {
+        auto length_str = str_format_time(length);
+        auto time_pad = length_str.len();
+
+        QString time_str;
         if (aud_get_bool ("qtui", "show_remaining_time"))
-            text = str_concat ({str_format_time (time - length), " / ", str_format_time (length)});
+        {
+            time = aud::max(0, length - time);
+            time_str = QString('-') + str_format_time(time);
+            time_pad++;
+        }
         else
-            text = str_concat ({str_format_time (time), " / ", str_format_time (length)});
+            time_str = str_format_time(time);
+
+        // To avoid the label changing width as time progresses, use
+        // monospaced digits and pad the time to the width of the song
+        // length (which should be the widest time we'll display).
+        text = "<b><tt>" + time_str.rightJustified(time_pad, QChar::Nbsp) +
+               "</tt> / <tt>" + length_str + "</tt></b>";
+    }
     else
-        text = str_format_time (time);
+        text = "<b><tt>" + QString(str_format_time(time)) + "</tt></b>";
 
     m_label->setText (text);
 }
@@ -101,7 +114,6 @@ void TimeSlider::start_stop ()
     bool ready = aud_drct_get_ready ();
     bool paused = aud_drct_get_paused ();
 
-    setEnabled (ready);
     m_label->setEnabled (ready);
 
     update ();
@@ -121,6 +133,7 @@ void TimeSlider::update ()
             int time = aud_drct_get_time ();
             int length = aud_drct_get_length ();
 
+            setEnabled (length >= 0);
             setRange (0, length);
             setValue (time);
 
@@ -129,6 +142,7 @@ void TimeSlider::update ()
     }
     else
     {
+        setEnabled (false);
         setRange (0, 0);
         set_label (0, 0);
     }
