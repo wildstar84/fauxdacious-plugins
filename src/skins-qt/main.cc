c8c3e6dadc3ce8137c1835b0edd45b38e4e40f9a
Jim Turner <turnerjw784@yahoo.com>
2021-11-23 21:51:05 -0600

    skins-qt - Fix minimize button to toggle playlist in AfterStep: (like skins-gtk does) when shaded & using the AfterStep window-manager, as for unknown reasons, it doesn't work, so we make it do the next best useful thing by toggling the playlist window (since that button isn't accessable when shaded)!

--- src/skins-qt/main.cc
+++ src/skins-qt/main.cc
@@ -694,6 +694,11 @@ static void mainwin_eq_cb (Button * button, QMouseEvent * event)
     { view_set_show_equalizer (button->get_active ()); }
 static void mainwin_pl_cb (Button * button, QMouseEvent * event)
     { view_set_show_playlist (button->get_active ()); }
+static void shaded_minimize_AS_cb (Button * button, QMouseEvent * event)
+    {
+        bool show = aud_get_bool ("skins", "playlist_visible");
+        view_set_show_playlist (! show);
+    }
 
 static void mainwin_spos_set_knob ()
 {
@@ -1082,18 +1087,13 @@ static void mainwin_create_widgets ()
     mainwin->put_widget (true, mainwin_shaded_menubtn, 6, 3);
     mainwin_shaded_menubtn->on_release ((ButtonCB) mainwin_menubtn_cb);
 
-    if (aud_get_bool ("audacious", "afterstep"))  /* JWT:THIS RICKITY OL' WINDOWMANAGER DON'T SEEM TO HANDLE iconify(): */
-    {   /* SO LET'S DO THE NEXT MOST USEFUL THING:  TOGGLE THE PLAYLIST! (I USE AS & *WANT* THIS!): */
-        mainwin_shaded_minimize = new Button (9, 9, 9, 0, 9, 0, 9, 0, 9, 0, SKIN_TITLEBAR, SKIN_TITLEBAR);
-        mainwin->put_widget (true, mainwin_shaded_minimize, 244, 3);
-        mainwin_shaded_minimize->on_release ((ButtonCB) mainwin_pl_cb);
-    }
+    mainwin_shaded_minimize = new Button (9, 9, 9, 0, 9, 9, SKIN_TITLEBAR, SKIN_TITLEBAR);
+    mainwin->put_widget (true, mainwin_shaded_minimize, 244, 3);
+    if (aud_get_bool ("audacious", "afterstep"))  /* JWT:AFTERSTEP DON'T SEEM TO HANDLE iconify() HERE, */
+        /* SO LET'S DO THE NEXT MOST USEFUL THING:  TOGGLE THE PLAYLIST! (I USE AS & *WANT* THIS!): */
+        mainwin_shaded_minimize->on_release ((ButtonCB) shaded_minimize_AS_cb);
     else
-    {
-        mainwin_shaded_minimize = new Button (9, 9, 9, 0, 9, 9, SKIN_TITLEBAR, SKIN_TITLEBAR);
-        mainwin->put_widget (true, mainwin_shaded_minimize, 244, 3);
         mainwin_shaded_minimize->on_release ((ButtonCB) mainwin_minimize_cb);
-    }
 
     mainwin_shaded_shade = new Button (9, 9, 0, 27, 9, 27, SKIN_TITLEBAR, SKIN_TITLEBAR);
     mainwin->put_widget (true, mainwin_shaded_shade, 254, 3);
