c39300d15cad47f248abbb278b4fc75046b3095b
Jim Turner <turnerjw784@yahoo.com>
2023-03-07 11:21:13 -0600

    aud - qtui: Search in file names with Ctrl+F. Addresses: #1204 (Audacious commit# 82ecc28, addresses Audacious feature req. #1204).

--- src/qtui/playlist_model.cc
+++ src/qtui/playlist_model.cc
@@ -356,7 +356,8 @@ bool PlaylistProxyModel::filterAcceptsRow (int source_row, const QModelIndex &) 
     String strings[] = {
         tuple.get_str (Tuple::Title),
         tuple.get_str (Tuple::Artist),
-        tuple.get_str (Tuple::Album)
+        tuple.get_str (Tuple::Album),
+        tuple.get_str (Tuple::Basename)
     };
 
     for (auto & term : m_searchTerms)
