2536a148472b8a032b6ddb23fd839d56bd7514f0
Jim Turner <turnerjw784@yahoo.com>
2024-05-16 22:42:24 -0500

    aud - scrobbler2: Make work on macOS and remove migration code. (Audacious commits# 895e2c3 & 04d1346)

--- src/scrobbler2/scrobbler.h
+++ src/scrobbler2/scrobbler.h
@@ -19,6 +19,7 @@
 
 //fauxdacious includes
 #include <libfauxdcore/i18n.h>
+#include <libaudcore/mainloop.h>
 #include <libfauxdcore/preferences.h>
 #include <libfauxdcore/runtime.h>
 #include <libfauxdcore/tuple.h>
@@ -56,15 +57,12 @@ extern pthread_mutex_t log_access_mutex;
 extern gboolean permission_check_requested;
 extern gboolean invalidate_session_requested;
 
-//Migrate the settings from the old scrobbler
-extern gboolean migrate_config_requested;
-
 //Send "now playing"
 extern gboolean now_playing_requested;
 extern Tuple now_playing_track;
 
 //scrobbler_communication.c
-extern gboolean   scrobbler_communication_init();
+extern gboolean scrobbler_communication_init();
 extern void * scrobbling_thread(void * data);
 
 /* Internal stuff */
