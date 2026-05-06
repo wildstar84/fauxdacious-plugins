6864e37f6f0109820994c62d68e0cf93deae3e38
Jim Turner <turnerjw784@yahoo.com>
2026-05-06 13:32:09 -0500

    lirc - Fix accidently-deleted semicolon

--- src/lirc/lirc.cc
+++ src/lirc/lirc.cc
@@ -127,7 +127,7 @@ void LIRCPlugin::cleanup ()
         if (input_tag)
         {
             g_source_remove (input_tag);
-            input_tag = 0
+            input_tag = 0;
         }
 
         lirc_freeconfig (config);
