dfb44dffd3b83c456a0b4330a13b22285784eae8
Jim Turner <turnerjw784@yahoo.com>
2026-06-04 22:17:05 -0500

    aud - skins[*]: Prompt to restart with Xwayland: - Make use of the new Xwayland configuration setting - Enable it automatically when attempting to use the Winamp interface on Wayland - Prompt to restart Audacious to take effect (Audacious commit #50a88eb)

--- src/skins/view.cc
+++ src/skins/view.cc
@@ -22,8 +22,14 @@
 #include "view.h"
 
 #include <libfauxdcore/hook.h>
+#include <libfauxdcore/i18n.h>
 #include <libfauxdcore/mainloop.h>
 #include <libfauxdcore/runtime.h>
+#include <libfauxdgui/libfauxdgui-gtk.h>
+
+#ifdef GDK_WINDOWING_WAYLAND
+#include <gdk/gdkwayland.h>
+#endif
 
 #include "plugin.h"
 #include "plugin-window.h"
@@ -41,6 +47,42 @@
 
 void view_show_player (bool show)
 {
+#ifdef GDK_WINDOWING_WAYLAND
+    static GtkWidget * dialog;
+
+    if (show && GDK_IS_WAYLAND_DISPLAY (gdk_display_get_default ()))
+    {
+        if (! dialog)
+        {
+            auto restart = audgui_button_new (_("Restart"), "view-refresh", [] (void *) {
+                aud_set_bool (nullptr, "use_xwayland", true);
+                aud_request_restart ();
+            }, nullptr);
+
+            auto quit = audgui_button_new (_("Quit"), "application-exit", [] (void *) {
+                aud_set_bool (nullptr, "use_xwayland", true);
+                aud_quit ();
+            }, nullptr);
+
+            dialog = audgui_dialog_new (GTK_MESSAGE_WARNING, _("Compatibility Issue"),
+                _("The Winamp interface requires windowing system features not "
+                "supported by Wayland. Fauxdacious will attempt to enable Xwayland "
+                "compatibility mode after restart.\n\n"
+                "With the GTK or Qt interface you may disable this setting "
+                "again in the \"Advanced\" category of the Fauxdacious settings."),
+                restart, quit);
+
+            g_signal_connect (dialog, "delete-event", (GCallback) aud_quit, nullptr);
+            g_signal_connect (dialog, "destroy", (GCallback) gtk_widget_destroyed, & dialog);
+        }
+
+        gtk_widget_show_all (dialog);
+        show = false; // don't show main window
+    }
+    else if (dialog)
+        gtk_widget_destroy (dialog);
+#endif
+
     if (show)
     {
         /* JWT:NEXT 2 MOVED HERE FROM plugin.cc:skins_init_main().
